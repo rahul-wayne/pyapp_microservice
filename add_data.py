ac874339b83cc3e38c81975659ca6ee8596a0550
Ubuntu <devops@dev-vm.kodkw00t0x4u5ihdstp4mghehb.tx.internal.cloudapp.net>
2024-06-15 14:52:56 +0000

    updatedasasas

---
+++ add_data.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, url_for, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Setup MongoDB connection
+client = MongoClient('mongodb://172.17.0.1:27017/')
+db = client['microserviceDB']
+collection = db['data']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data', methods=['GET', 'POST'])
+def add_data():
+    if request.method == 'POST':
+        data = request.form
+        collection.insert_one({'title': data['title'], 'content': data['content']})
+        return redirect(url_for('add_data'))
+    return render_template('add_data.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
---
+++ view_data.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
+from bson import ObjectId
+
+app = Flask(__name__)
+
+# Setup MongoDB connection
+client = MongoClient('mongodb://172.17.0.1:27017/')
+db = client['microserviceDB']
+collection = db['data']
+
+@app.route('/view')
+def view_data():
+    data = list(collection.find())
+    for item in data:
+        item['_id'] = str(item['_id'])
+    return render_template('view_data.html', data=data)
+
+@app.route('/delete/<id>', methods=['GET'])
+def delete_data(id):
+    collection.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('view_data'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5002)
